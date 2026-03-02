63daf85529b818c663f6a3c25a5042e755acf263
kartavya-12 <kartavya03784@gmail.com>
2026-03-02 07:32:44 +0530

    assignment

--- 2/script.js
+++ 2/script.js
@@ -1,15 +1,112 @@
-const form=document.querySelector("#weather_form")
-const API_KEY= "0133cc5316757ac730cc46ae342334e4"
+let API_KEY = "ea0beec1a1a15e6771b7ecdc73f830ec";
 
-form.addEventListener("submit", async function(e){
-    e.preventDefault();
-    console.log(city.value)
-    const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`)
-    const weatherData= await response.json()
-    console.log(weatherData)
-    console.log("city",weatherData.name)
-    console.log("temp",(weatherData.main.temp-273).toFixed(1),"c")
-    console.log("weather",weatherData.weather[0].main)
-    console.log("humidity",weatherData.main.humidity)
-    console.log("wind",weatherData.wind.speed,"m/s")
-})
+        let form = document.querySelector('form')
+        let city = document.querySelector("#search-city")
+        let weather = document.querySelector('#fetched-data-box')
+        let searchcity = document.querySelector('#search-cities-box')
+        let darkBtn = document.getElementById("dark-toggle")
+        let clearAllBtn = document.getElementById("clear-all");
+
+
+        async function getWeatherInfo(city) {
+
+            if (city.trim() === "") return;
+
+            let response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+            );
+
+            let data = await response.json();
+
+            if (data.cod !== 200) {
+                weather.innerHTML = "<p>City not found</p>";
+                return;
+            }
+
+            let card = document.createElement('div');
+            card.innerHTML = `
+        <div><b>City :</b> ${data.name}, ${data.sys.country}</div>
+        <div><b>Temp :</b> ${data.main.temp}°C</div>
+        <div><b>Weather :</b> ${data.weather[0].main}</div>
+        <div><b>Humidity :</b> ${data.main.humidity}%</div>
+        <div><b>Wind :</b> ${data.wind.speed} m/s</div>
+    `;
+
+            weather.innerHTML = "";
+            weather.append(card);
+
+            let cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+            if (!cities.includes(city)) {
+                cities.push(city);
+                localStorage.setItem("cities", JSON.stringify(cities));
+            }
+
+            displayHistory();
+        }
+
+        function displayHistory() {
+            let cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+            searchcity.innerHTML = "";
+
+            cities.forEach((cityName, index) => {
+
+                let item = document.createElement("div");
+                item.classList.add("history-item");
+
+                let cityText = document.createElement("span");
+                cityText.textContent = cityName;
+
+                cityText.addEventListener("click", () => {
+                    getWeatherInfo(cityName);
+                });
+
+                let deleteBtn = document.createElement("button");
+                deleteBtn.textContent = "✖";
+                deleteBtn.classList.add("delete-btn");
+
+                deleteBtn.addEventListener("click", () => {
+                    cities.splice(index, 1);
+                    localStorage.setItem("cities", JSON.stringify(cities));
+                    displayHistory();
+                });
+
+                item.appendChild(cityText);
+                item.appendChild(deleteBtn);
+
+                searchcity.appendChild(item);
+            });
+        }
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            getWeatherInfo(city.value)
+        })
+        displayHistory();
+        clearAllBtn.addEventListener("click", () => {
+
+            localStorage.removeItem("cities");
+
+            weather.innerHTML = "<p>No city selected.</p>";
+            currentDisplayedCity = null;
+
+            displayHistory();
+        });
+
+        if (localStorage.getItem("theme") === "dark") {
+            document.body.classList.add("dark");
+            darkBtn.textContent = "Light Mode";
+        }
+
+        darkBtn.addEventListener("click", () => {
+            document.body.classList.toggle("dark");
+
+            if (document.body.classList.contains("dark")) {
+                localStorage.setItem("theme", "dark");
+                darkBtn.textContent = "Light Mode";
+            } else {
+                localStorage.setItem("theme", "light");
+                darkBtn.textContent = "Dark Mode";
+            }
+        });
